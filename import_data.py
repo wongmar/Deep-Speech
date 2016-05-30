d1ff480bdc6b335daabeb81d9154b89357e90ce4
wongmar <wongmar@users.noreply.github.com>
2016-05-30 12:33:58 -0700

    Added base files

---
+++ import_data.py
@@ -0,0 +1,302 @@
+"""Functions for downloading and reading MNIST data."""
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+from numpy import array
+import tensorflow as tf
+import csv
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+from tensorflow.python.platform import gfile
+
+def _read32(bytestream):
+  dt = np.dtype(np.uint32).newbyteorder('>')
+  return np.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+
+#training data is one file per utterance of form:
+#label
+#[1,2,3,4,...,x] - spectrogram for nth window
+#[1,2,3,4,...,x] - logs
+#1				 - energy
+#[1,2,3,4,...,x] - standard histogram
+#*				 - divider
+#etc
+def read_training_data(filename,n_steps,training=True):
+	print("reading file: ",filename)
+	spectrogram = []
+	logs = []
+	energy = []
+	histogram = []
+	revolve = 0
+	first = True
+	with open(filename, 'rb') as csvfile:
+		data = csv.reader(csvfile, delimiter=',') 
+		for row in data: #every row is a row of the csv. without formatting each array looks like ['1','2',...,'n'] #data prints the csvreader object
+						 #every row should be an array of time slices for a single utterance
+			#print(row)
+			if training==True:
+				if first:
+					label = row
+					first = False
+				elif revolve == 0:
+					revolve+=1
+				elif revolve == 1:
+					p = [s.strip('[]') for s in row]
+					p = [float(s) for s in p]
+					spectrogram.append(p)
+					revolve+=1
+				elif revolve == 2:
+					l = [s.strip('[]') for s in row]
+					logs.append(l)
+					revolve+=1
+				elif revolve == 3:
+					e = [s.strip('[]') for s in row]
+					energy.append(e)
+					revolve += 1
+				elif revolve == 4:
+					h = [s.strip('[]') for s in row]
+					histogram.append(h)
+					revolve = 0
+				else:
+					print("else")
+					revolve = 1;
+			else:
+				label = ""
+				if revolve == 0:
+					p = [s.strip('[]') for s in row]
+					p = [float(s) for s in p]
+					spectrogram.append(p)
+					revolve+=1
+				elif revolve == 1:
+					l = [s.strip('[]') for s in row]
+					logs.append(l)
+					revolve+=1
+				elif revolve == 2:
+					e = [s.strip('[]') for s in row]
+					energy.append(e)
+					revolve += 1
+				elif revolve == 3:
+					h = [s.strip('[]') for s in row]
+					histogram.append(h)
+					revolve +=1
+				else:
+					revolve = 0;
+	csvfile.close()
+	test = np.array(spectrogram)
+	test.resize(n_steps,160) #160 for 16000hz, can be changed
+	#print(test.shape)
+	return label,test
+	
+
+def read_spectrogram(filename):
+	print("Reading Spectrogram: ",filename)
+	out = []
+	with open(filename, 'rb') as csvfile:
+		data = csv.reader(csvfile, delimiter='[',lineterminator=']') 
+		for row in data: #every row is a row of the csv. without formatting each array looks like ['1','2',...,'n'] #data prints the csvreader object
+						 #every row should be an array of time slices for a single utterance
+			out.append(row)
+	#print(out)
+	return out
+
+def dense_to_one_hot(labels_dense, num_classes):
+  """Convert class labels from scalars to one-hot vectors."""
+  num_labels = labels_dense.shape[0]
+  index_offset = np.arange(num_labels) * num_classes
+  labels_one_hot = np.zeros((num_labels, num_classes))
+  labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+  return labels_one_hot
+
+
+#start is the column which is before the start of the label, in our case for ted-lium data the transcript begins on the 7th column
+def read_labels(filename, start=6):
+	print("Reading Label: ",filename)
+	out = []
+	with open(filename, 'rb') as csvfile:
+		data = csv.reader(csvfile, delimiter=' ')
+		for row in data: #every row should be a string which represents the label
+			out.append(" ".join(row[start:]))
+	out = np.asarray(out)
+	return out
+
+
+
+def read_dir(train_dir, batch_size,n_steps, training=True):
+	speech = []
+	labels = []
+	i=0
+	files = [f for f in listdir(train_dir) if isfile(train_dir+'/'+f)]
+	for file in files:
+		''' 
+		#Causes index out of bounds errors
+		a,b = read_training_data(train_dir+'/'+file,n_steps, training)
+		test = np.array(b)
+		labels.append(a)
+		speech.append(b)
+		'''
+		if i<batch_size:
+			a,b = read_training_data(train_dir+'/'+file,n_steps, training)
+			test = np.array(b)
+			#print(test.shape)
+			labels.append(a)
+			speech.append(b)
+			i+=1
+		else:
+			break
+		
+	
+#	speechfiles = [f for f in listdir(speech_path) if isfile(join(speech_path, f))]
+#	for file in speechfiles:
+#		speech.append(read_training_data(speech_path+file))
+	#print(len(speech))
+	#print(len(speech[0]))
+	#print(len(speech[0][0]))
+	speech = np.array(speech)
+	#speech.shape=(10,1)
+	#speech.shape = (len(speech),len(speech[0]), len(speech[0][0]))
+	#print(speech.shape)
+	#speech.shape = (data_size,num_windows,spectrogram_size)
+#	
+#	labelfiles = [f for f in listdir(label_path) if isfile(join(label_path, f))]
+#	for file in labelfiles:
+#		labels.append(read_labels(label_path+file))
+	labels = np.array(labels)	
+	#print(speech)
+	return speech,labels
+	
+class DataSet(object):
+
+  def __init__(self,
+               images,
+               labels,
+               fake_data=False,
+               one_hot=False,
+               dtype=dtypes.float32):
+    """Construct a DataSet.
+    one_hot arg is used only if fake_data is true.  `dtype` can be either
+    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+    `[0, 1]`.
+    """
+    dtype = dtypes.as_dtype(dtype).base_dtype
+    if dtype not in (dtypes.uint8, dtypes.float32):
+      raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
+                      dtype)
+    if fake_data:
+      self._num_examples = 10000
+      self.one_hot = one_hot
+    else:
+      assert images.shape[0] == labels.shape[0], (
+          'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+      self._num_examples = images.shape[0]
+
+      # Convert shape from [num examples, rows, columns, depth]
+      # to [num examples, rows*columns] (assuming depth == 1)
+      #assert images.shape[3] == 1
+      #images = images.reshape(images.shape[0],
+      #                       images.shape[2])
+      if dtype == dtypes.float32:
+        # Convert from [0, 255] -> [0.0, 1.0].
+        images = images.astype(np.float32)
+        #images = np.multiply(images, 1.0 / 255.0)
+    self._images = images
+    self._labels = labels
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def images(self):
+    return self._images
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  def next_batch(self, batch_size, fake_data=False):
+    """Return the next `batch_size` examples from this data set."""
+    if fake_data:
+      fake_image = [1] * 784
+      if self.one_hot:
+        fake_label = [1] + [0] * 9
+      else:
+        fake_label = 0
+      return [fake_image for _ in xrange(batch_size)], [
+          fake_label for _ in xrange(batch_size)
+      ]
+    start = self._index_in_epoch
+    self._index_in_epoch += batch_size
+    if self._index_in_epoch > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Shuffle the data
+      perm = np.arange(self._num_examples)
+      np.random.shuffle(perm)
+      self._images = self._images[perm]
+      self._labels = self._labels[perm]
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size
+      assert batch_size <= self._num_examples
+    end = self._index_in_epoch
+    return self._images[start:end], self._labels[start:end]
+
+
+def read_data_sets(train_dir, batch_size, n_steps, training=True,
+                   fake_data=False,
+                   one_hot=False,
+                   dtype=dtypes.float32):
+  if fake_data:
+
+    def fake():
+      return DataSet([], [], fake_data=True, one_hot=one_hot, dtype=dtype)
+
+    train = fake()
+    validation = fake()
+    test = fake()
+    return base.Datasets(train=train, validation=validation, test=test)
+
+  train_speech,train_labels = read_dir(train_dir, batch_size,n_steps, training)
+  
+
+  train = DataSet(train_speech, train_labels, dtype=dtype)
+
+  return train
+
+
+def load_mnist():
+  return read_data_sets('MNIST_data')
+  
+#if __name__ == '__main__':
+  #read_spectrogram("test.txt")
+  #read_labels("911Mothers_2010W.stm",6)
+  #read_data_sets("train",10,5000)
---
+++ speech.py
@@ -0,0 +1,253 @@
+import tensorflow as tf
+from tensorflow.python.ops.constant_op import constant
+#from tensorflow.nn.rnn_ import rnn, rnn_cell
+from tensorflow.python.ops import functional_ops
+import numpy as np
+import sys
+
+import import_data
+
+def charToInt(c):
+	if c == ' ':
+		return 26
+	else:
+		return ord(c) - ord('a')
+
+
+#Engine
+t_flag = 1 #training flag
+learning_rate = 0.0001
+training_iters = 100
+batch_size = 2
+display_step = 10
+dropout_rate = 0.05
+relu_clip = 20
+n_steps = 500
+n_input = 160
+n_context = 0 #Since we are already creating overlapping spectrograms, this does not need to be set.
+
+n_hidden_1 = n_input + 2*n_input*n_context 
+n_hidden_2 = n_input + 2*n_input*n_context 
+n_hidden_5 = n_input + 2*n_input*n_context
+
+n_cell_dim = n_input + 2*n_input*n_context
+n_hidden_3 = 2 * n_cell_dim
+
+n_character = 28
+
+n_hidden_6 = n_character
+
+x = tf.placeholder("float", [None, n_steps, n_input + 2*n_input*n_context])
+y = tf.placeholder("string", [None,1])
+#y = tf.placeholder("int", [None,n_steps])
+z = tf.placeholder(tf.int32, [None, n_steps])
+
+istate_fw = tf.placeholder("float", [None, 2*n_cell_dim])
+istate_bw = tf.placeholder("float", [None, 2*n_cell_dim])
+
+keep_prob = tf.placeholder(tf.float32)
+keep_prob = 0.05
+
+
+weights = {
+	'h1': tf.Variable(tf.random_normal([n_input + 2*n_input*n_context, n_hidden_1])),
+	'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+	'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
+	'h5': tf.Variable(tf.random_normal([(2 * n_cell_dim), n_hidden_5])),
+	'h6': tf.Variable(tf.random_normal([n_hidden_5, n_hidden_6]))
+}
+biases = {
+	'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+	'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+	'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+	'b5': tf.Variable(tf.random_normal([n_hidden_5])),
+	'b6': tf.Variable(tf.random_normal([n_hidden_6]))
+}
+
+def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases):
+	# Input shape: [batch_size, n_steps, n_input + 2*n_input*n_context]
+	_X = tf.transpose(_X, [1, 0, 2])  # Permute n_steps and batch_size
+	# Reshape to prepare input for first layer
+	_X = tf.reshape(_X, [-1, n_input + 2*n_input*n_context]) # (n_steps*batch_size, n_input + 2*n_input*n_context)
+
+	#Hidden layer with clipped RELU activation and dropout
+	layer_1 = tf.minimum(tf.nn.relu(tf.add(tf.matmul(_X, _weights['h1']), _biases['b1'])), relu_clip)
+	layer_1 = tf.nn.dropout(layer_1, keep_prob)
+	#Hidden layer with clipped RELU activation and dropout
+	layer_2 = tf.minimum(tf.nn.relu(tf.add(tf.matmul(layer_1, _weights['h2']), _biases['b2'])), relu_clip)
+	layer_2 = tf.nn.dropout(layer_2, keep_prob)
+	#Hidden layer with clipped RELU activation and dropout
+	layer_3 = tf.minimum(tf.nn.relu(tf.add(tf.matmul(layer_2, _weights['h3']), _biases['b3'])), relu_clip)
+	layer_3 = tf.nn.dropout(layer_3, keep_prob)
+
+	# Define lstm cells with tensorflow
+	# Forward direction cell
+	lstm_fw_cell = tf.nn.rnn_cell.BasicLSTMCell(n_cell_dim, forget_bias=1.0)
+	# Backward direction cell
+	lstm_bw_cell = tf.nn.rnn_cell.BasicLSTMCell(n_cell_dim, forget_bias=1.0)
+
+	# Split data because rnn cell needs a list of inputs for the BRNN inner loop
+	layer_3 = tf.split(0, n_steps, layer_3)
+
+	# Get lstm cell output
+	#						outputs is a tuple, must change to tensor
+	outputs = tf.nn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, layer_3, initial_state_fw=_istate_fw, initial_state_bw=_istate_bw) 
+	
+	# Reshape outputs from a list of n_steps tensors each of shape [batch_size, 2*n_cell_dim]
+	# to a single tensor of shape [n_steps*batch_size, 2*n_cell_dim]
+	outputs = tf.pack(outputs[0])
+	outputs = tf.reshape(outputs, [-1, 2*n_cell_dim])
+	#Hidden layer with clipped RELU activation and dropout
+	layer_5 = tf.minimum(tf.nn.relu(tf.add(tf.matmul(outputs, _weights['h5']), _biases['b5'])), relu_clip)
+	layer_5 = tf.nn.dropout(layer_5, keep_prob)
+	
+	#Hidden layer with softmax function
+	layer_6 = tf.nn.softmax(tf.add(tf.matmul(layer_5, _weights['h6']), _biases['b6']))
+
+	# Reshape layer_6 from a tensor of shape [n_steps*batch_size, n_hidden_6]
+	# to a tensor of shape [batch_size, n_steps, n_hidden_6]
+	layer_6 = tf.reshape(layer_6, [n_steps, batch_size, n_hidden_6])
+	#layer_6 = tf.transpose(layer_6, [1, 0, 2])  # Permute n_steps and batch_size
+	
+	# Return layer_6
+	return layer_6
+	
+pred = BiRNN(x, istate_fw, istate_bw, weights, biases)
+
+
+
+#Training
+
+def SimpleSparseTensorFrom(x):
+  """Create a very simple SparseTensor with dimensions (batch, time).
+  Args:
+    x: a list of lists of type int
+  Returns:
+    x_ix and x_val, the indices and values of the SparseTensor<2>.
+  """
+  x_ix = []
+  x_val = []
+  for batch_i, batch in enumerate(x):
+    for time, val in enumerate(batch):
+      x_ix.append([batch_i, time])
+      x_val.append(val)
+  x_shape = [len(x), np.asarray(x_ix).max(0)[1]+1]
+  x_ix = tf.constant(x_ix, tf.int64)
+  x_val = tf.constant(x_val, tf.int32)
+  x_shape = tf.constant(x_shape, tf.int64)
+
+  return tf.SparseTensor(x_ix, x_val, x_shape)
+
+
+mnist = import_data.read_data_sets("data", batch_size,n_steps)
+labs = []
+for a in mnist.labels:
+	tmp = [charToInt(c) for c in a[0]]
+	labs.append(tmp)
+labs += [] * (batch_size - len(labs))
+for i in xrange(len(labs)-1):
+	labs[i] += [0] * (n_steps - len(labs[i]))
+
+lab = SimpleSparseTensorFrom(labs)
+
+'''
+#An attempt to have the program not read the data set twice. Since a tensor cannot be modified or read outside of a session, this function failed to work
+def create_label(z):
+	labs = []
+	for a in z:
+		tmp = [charToInt(c) for c in a[0]]
+		labs.append(tmp)
+
+	for i in xrange(len(labs)):
+		labs[i] += [0] * (n_steps - len(labs[i]))
+	labs += [''] * (batch_size - len(labs))
+
+
+	#lab = tf.SparseTensor(indices=index, values=new_labs, shape=sh)
+	lab = SimpleSparseTensorFrom(labs)
+	return lab
+'''
+
+# Define loss and optimizer
+#labels needs to be a SparseTensor of i,[(b,t)] where i is the id of batch b, time t
+#batch b is which batch, time t is which step?
+s_length = np.full(batch_size,n_steps, dtype=np.int64)
+
+print("Defining Cost")
+#cost =  tf.reduce_mean(tf.contrib.ctc.ctc_loss(inputs=pred, labels=lab, sequence_length=s_length))
+cost =  tf.reduce_mean(tf.contrib.ctc.ctc_loss(inputs=pred, labels=y, sequence_length=s_length))
+
+print("Defining Optimizer")
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost,aggregation_method=2) # Adam Optimizer
+
+# Evaluate model ~~~~~DECODE~~~~~
+#correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+print("Defining Correct Label")
+correct_pred = tf.argmax(pred,1)
+
+print "Defining Accuracy"
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+	
+print("Initializing Variables")
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+
+
+# Launch the graph
+with tf.Session() as sess:
+	sess.run(init)
+	saver = tf.train.Saver(weights)
+	step = 1
+	#print("if")
+	# Keep training until reach max iterations
+	
+	if t_flag == 1:
+		print("Start Training")
+		mnist = import_data.read_data_sets("data", batch_size,n_steps)
+		while step * batch_size < training_iters:
+			batch_xs, batch_ys = mnist.next_batch(batch_size)
+			# Reshape data
+			xs_shaped = batch_xs.copy()
+			xs_shaped.resize(batch_size,n_steps, n_input + 2*n_input*n_context)
+			
+			
+			labs = []
+			for a in batch_ys:
+				tmp = [charToInt(c) for c in a[0]]
+				labs.append(tmp)
+			labs += [] * (batch_size - len(labs))
+			for i in xrange(len(labs)-1):
+				labs[i] += [0] * (n_steps - len(labs[i]))
+
+			lab = SimpleSparseTensorFrom(labs)
+			
+			
+			print "Running Optimizer"
+			sess.run(optimizer, feed_dict={x: xs_shaped, y: batch_ys, z:labs,
+											istate_fw: np.zeros((batch_size, 2*n_cell_dim)),
+											istate_bw: np.zeros((batch_size, 2*n_cell_dim))})
+			if step % display_step == 0:
+				# Calculate batch accuracy
+				print "Evaluate Accuracy"
+				acc = sess.run(accuracy, feed_dict={x: xs_shaped, y: batch_ys,
+													istate_fw: np.zeros((batch_size, 2*n_cell_dim)),
+													istate_bw: np.zeros((batch_size, 2*n_cell_dim))})
+				# Calculate batch loss
+				print "Evaluate Loss"
+				loss = sess.run(cost, feed_dict={x: xs_shaped, y: batch_ys,
+												 istate_fw: np.zeros((batch_size, 2*n_cell_dim)),
+												 istate_bw: np.zeros((batch_size, 2*n_cell_dim))})
+				print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
+													", Training Accuracy= " + "{:.5f}".format(acc)
+			step += 1
+			print "Step Completed"
+		print "Optimization Finished!"
+		save_path = saver.save(sess, "weights.ckpt")
+		print("Model saved in file: %s" % save_path)
+	if t_flag == 0:
+		mnist = import_data.read_data_sets("data",1,n_steps,training=False)
+		prediction=tf.argmax(pred,1)
+		print "predictions", prediction.eval(feed_dict={x: mnist.images, y:mnist.labels[0], istate_fw: np.zeros((batch_size, 2*n_cell_dim)),
+													istate_bw: np.zeros((batch_size, 2*n_cell_dim))}, session=sess)
+	sess.close()
